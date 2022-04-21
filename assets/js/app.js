5c988dc26f53009f4045bfe88794addae75a6013
nehabht <ne.bhatti2@icloud.com>
2022-04-21 14:05:34 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: "#app",
+
+    data:{
+
+    }
+
+})
+
+
+/* MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container 
+e inseriamo l'immagine grande a sinistra e le thumbnails sulla destra in modo da poter stilare lo slider; 
+avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico. */
